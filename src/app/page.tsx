5802e6717ab594e8963cbd25a50b49ec3bfb15a5
SCIACCA ALFIO <davy.torrisi1994@gmail.com>
2025-09-21 16:06:41 +0200

    style(prettier): format workflow, compose and page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
-export default function Home(){
-  return(
-    <main style={{padding: 24}}>
+export default function Home() {
+  return (
+    <main style={{ padding: 24 }}>
       <h1>Hello Full-Stack</h1>
     </main>
   );
